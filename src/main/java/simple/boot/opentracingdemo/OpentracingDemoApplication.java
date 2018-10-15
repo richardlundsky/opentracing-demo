4f0fdf4aa8f9d3d79f1a38230d2bd7d778a79b30
lundric <richard.lund@sky.uk>
2018-10-15 13:53:33 +0100

    Simple with no tracing

--- src/main/java/simple/boot/opentracingdemo/OpentracingDemoApplication.java
+++ src/main/java/simple/boot/opentracingdemo/OpentracingDemoApplication.java
@@ -1,6 +1,5 @@
 package simple.boot.opentracingdemo;
 
-import io.jaegertracing.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -13,12 +12,6 @@ public class OpentracingDemoApplication {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 		return restTemplateBuilder.build();
-	}
-
-	@Bean
-	public io.opentracing.Tracer jaegerTracer() {
-		return Configuration.fromEnv().
-				getTracer();
 	}
 
 	public static void main(String[] args) {
